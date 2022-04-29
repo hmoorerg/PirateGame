4c90154a6775ca932b26a44abe926e1fc170b470
Henry Moore <hmoorerg@gmail.com>
2022-04-29 14:04:44 -0700

    Added music files and jukebox and combined game elements

---
+++ Assets/RandomMusicPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMusicPlayer : MonoBehaviour
+{
+
+    public AudioClip[] Songs;
+    private AudioSource _audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_audioSource.isPlaying) 
+        {
+
+            PlayRandomSong();
+        }
+        
+    }
+    
+    void PlayRandomSong()
+    {
+        var randomSong = Songs[Random.Range(0,Songs.Length)];
+
+        _audioSource.clip = randomSong; 
+        _audioSource.Play();
+    }
+}
