b11158be675fe07bdc88ddc46efdf07b143e9682
Henry Moore <hmoorerg@gmail.com>
2022-06-03 00:46:22 -0700

    Adds restart button to the death screen

---
+++ Assets/RestartGameButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestartGameButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Add a listener to the OnClick event
+        GetComponent<Button>().onClick.AddListener(() => {
+            // Reload the level
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Procedural");
+        });
+    }
+}
